7f835aa8763fe037ce74ead4d0edfd6d51faf203
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2023-08-03 01:40:16 +0300

    Routes, Notifications & Authentications

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TweetController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +20,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(['user' => $request->user()]);
 });
+
+// Auth::routes();
+
+Route::post('auth/register', [UsersController::class, 'store']);
+Route::post('/login', [UsersController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/tweet/store', [TweetController::class, 'store']);
