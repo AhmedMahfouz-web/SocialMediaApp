c6f75f3a8bd3ff619402ee62118f4f37af0ebc62
Mariam Khairy <112273708+mariam-khairy@users.noreply.github.com>
2023-08-05 12:48:38 +0300

    Update ChatController.php
    Chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -10,27 +10,48 @@ class ChatController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+   
+    public function index(Request $request)
     {
-        //
-    }
+       // Retrieve chat messages
+       $chats = Chat::with('sender', 'reveiver', 'tweet')
+       ->where(function ($query) use ($request) {
+           $query->where('sender_id', $request->user_id)
+               ->orWhere('reveiver_id', $request->user_id);
+       })
+       ->orderBy('created_at')
+       ->get();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
+   return response()->json($chats);
+}
+     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        // Create a new chat message
+        $chat = Chat::create([
+            'sender_id' => $request->sender_id,
+            'reveiver_id' => $request->reveiver_id,
+            'message' => $request->message,
+            'tweet_id' => $request->tweet_id,
+        ]);
+
+        return response()->json($chat, 201);
     }
 
+    //Get message from sender to reciver
+    public function getReceiverMessages(Request $request)
+{
+    $receiverId = $request->receiver_id;
+
+    $messages = Chat::where('reveiver_id', $receiverId)
+        // ->with('sender', 'receiver', 'tweet')
+        ->orderBy('created_at')
+        ->get();
+
+    return response()->json($messages);
+}
     /**
      * Display the specified resource.
      */
